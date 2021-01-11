7e76d05c97d2af258d8235dd9dc420fb93e54fdb
jiwonson1 <jiwonson.dev@gmail.com>
2021-01-11 15:41:27 +0900

    algorythm study_python

---
+++ 기초 if~else/1066 [기초-조건] 짝홀 출력.py
@@ -0,0 +1,16 @@
+﻿a,b,c=input().split()
+x =int(a)
+y =int(b)
+z =int(c)
+if x%2==0:
+    print("even")
+else:
+    print("odd")
+if y%2==0:
+    print("even")
+else:
+    print("odd")
+if z%2==0:
+    print("even")
+else:
+    print("odd")
---
+++ 기초 if~else/1068 [기초-조건] 정수 1개.py
@@ -0,0 +1,11 @@
+﻿a = input()
+x = int(a)
+if x >=90:
+    print("A")
+elif x >= 70:
+    print("B")
+elif x>=40:
+    print("C")
+else:
+    print("D")
+
