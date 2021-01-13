fa6fa9472fb9e3aed999e1c2954055b3bd4fb963
jiwonson1 <jiwonson.dev@gmail.com>
2021-01-14 00:26:20 +0900

    study if else

---
+++ 기초 if~else/1163 [기초-조건] 사주보기2.py
@@ -0,0 +1,9 @@
+﻿x,y,z=map(int,input().split())
+
+n =(x+y+z)%1000
+n = int(n/100)
+
+if(n%2) ==0:
+    print('대박')
+else:
+    print('그럭저럭')
---
+++ 기초 if~else/1164 [기초-조건] 터널통과.py
@@ -0,0 +1,9 @@
+﻿x,y,z=map(int,input().split())
+if(x <=170):
+    print('CRASH')
+elif(y <=170):
+    print('CRASH')
+elif(z <=170):
+    print('CRASH')
+else:
+    print('PASS')
