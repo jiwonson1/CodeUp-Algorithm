8597949b3e455e77958d7126139a836c64ee78b3
jiwonson1 <jiwonson.dev@gmail.com>
2021-01-26 21:11:59 +0900

    중첩반복문

---
+++ 기초- 중첩 반복문/1356 사각형 출력하기2.py
@@ -0,0 +1,14 @@
+﻿n = int(input())
+
+for i in range(1, n+1):
+    if (i == 1) or (i == n):
+        for j in range(1, n+1):
+            print('*', end='')
+        print()
+    else:
+        print('*', end='')
+        for j in range(1, n-1):
+            print(' ', end = '')
+        print('*', end ='')
+        print()
+    
---
+++ 기초- 중첩 반복문/1357 삼각형 출력하기4.py
@@ -0,0 +1,10 @@
+﻿n = int(input())
+
+for i in range(1, n+1):
+    for j in range(0, i):
+        print('*', end='')
+    print()
+for i in range(1, n+1):
+    for j in range(i,n):
+        print('*', end='')
+    print()
