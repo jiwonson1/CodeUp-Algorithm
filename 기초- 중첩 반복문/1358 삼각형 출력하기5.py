ae8a9bb96f22c3b087c363bd308c10dce1d1501a
jiwonson1 <jiwonson.dev@gmail.com>
2021-01-28 00:03:11 +0900

    중첩반복문

---
+++ 기초- 중첩 반복문/1358 삼각형 출력하기5.py
@@ -0,0 +1,10 @@
+﻿n = int(input())
+
+for i in range(int(n/2), -1, -1):
+    for j in range(0, i):
+        print(' ', end='')
+    for j in range(0, (n - i * 2)):
+        print('*', end='')
+    for j in range(0, i):
+        print(' ', end='')
+    print()
