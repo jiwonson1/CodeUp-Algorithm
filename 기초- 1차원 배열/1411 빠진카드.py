f2f14b401c79409213748fa009cefae2dd08a20c
jiwonson1 <jiwonson.dev@gmail.com>
2021-02-11 20:14:45 +0900

    1차원 배열

---
+++ 기초- 1차원 배열/1411 빠진카드.py
@@ -0,0 +1,16 @@
+﻿n = int(input())
+cards = []
+miss = []
+
+for i in range(1, n+1):
+    cards.append(i)   
+    
+for i in range(0, n-1):
+    num = int(input())
+    miss.append(num)  
+
+for card in miss:
+    cards.remove(card) 
+    
+print(cards[0])
+
