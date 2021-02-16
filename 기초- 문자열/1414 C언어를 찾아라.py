baf6e82cab56c5f08deb6469c84eab8c7a446322
jiwonson1 <jiwonson.dev@gmail.com>
2021-02-16 23:55:49 +0900

    문자열

---
+++ 기초- 문자열/1414 C언어를 찾아라.py
@@ -0,0 +1,13 @@
+﻿string = input()
+string = string.upper()
+c = 0
+cc = 0
+
+for i in range(0, len(string)):
+    if string[i] == 'C':
+        c += 1
+        if i != len(string)-1 and string[i+1] == 'C':
+            cc += 1
+
+print(c)
+print(cc)
