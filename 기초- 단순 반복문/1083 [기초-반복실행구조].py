ff86cac1f4cb44f2cfe505f684033f442e42d94a
jiwonson1 <jiwonson.dev@gmail.com>
2021-10-23 15:56:23 +0900

    update algorytm by java

--- 기초- 단순 반복문/1083 [기초-반복실행구조].py
+++
@@ -1,6 +0,0 @@
-﻿a = int(input())
-for i in range(1,a+1) :
-    if (i%3==0):
-        print("X", end = ' ')
-    else : 
-        print(i, end = ' ')
---
+++ 기초- 문자열/1990 3의 배수 판별 .java
@@ -0,0 +1,30 @@
+﻿import java.util.Scanner;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String n = scan.next();
+        
+        int sum = 0;
+        
+        //n의 글자 수 만큼 배열을 생성한다.
+        char[] num = new char[n.length()];
+        
+        //3의 배수 구하는 공식
+        for(int i=0; i<num.length; i++){
+            num[i] = n.charAt(i);
+            sum+=num[i];
+        }
+        
+        //3으로 나눠지면 3의 배수
+        if(sum % 3 == 0){
+            System.out.print("1");
+        }else{
+            System.out.print("0");
+        }
+        
+        
+    }
+    
+}
