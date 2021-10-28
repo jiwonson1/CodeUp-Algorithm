267618352959b129026eda19415ee220fc00466d
jiwonson1 <jiwonson.dev@gmail.com>
2021-10-28 14:02:27 +0900

    practice by java

---
+++ 기초- 2차원 배열/1460 - 2차원 배열 순서대로 채우기1-1.java
@@ -0,0 +1,22 @@
+﻿import java.util.*;
+
+public class Main{
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = n*n;
+        
+        for(int i=1; i<x+1; i++){
+    
+            if(i%n == 0){
+                System.out.println(i);
+            }else{
+                System.out.print(i+" ");
+            }
+    
+        }
+        
+    }
+    
+}
