47210f73b183fcdaef7151adba53a8d96492d311
jiwonson1 <jiwonson.dev@gmail.com>
2021-10-31 01:31:58 +0900

    practice by java

---
+++ 기초- 2차원 배열/1462 - 2차원 배열 순서대로 채우기 1-3.java
@@ -0,0 +1,19 @@
+﻿import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int[][] b = new int[a][a];
+		int c=1;
+		for (int i = 0; i < b.length; i++) {
+			for (int j = 0; j < b.length; j++) {
+				b[i][j]+=c;
+				System.out.print(b[i][j]+" ");
+				c+=a;
+			}
+			c=i+2;
+			System.out.println();
+		}
+	}//main end 
+}
