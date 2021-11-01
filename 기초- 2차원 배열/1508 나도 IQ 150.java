9927b39f0991ae373bc5424ca278ae4c46636ef3
jiwonson1 <jiwonson.dev@gmail.com>
2021-11-01 22:42:15 +0900

    paractice by java

---
+++ 기초- 2차원 배열/1508 나도 IQ 150.java
@@ -0,0 +1,26 @@
+﻿import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[100][100];
+		for(int i = 0; i < n; i++) {
+			int k = sc.nextInt();
+			for(int j = 0; j < i+1; j++) {
+				arr[i][j] = k;
+				if(j >= 1) {
+					arr[i][j] = arr[i][j-1] - arr[i-1][j-1];
+				}
+			}
+		}
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < i+1; j++) {
+				System.out.printf("%d ", arr[i][j]);
+			}
+			System.out.println("");
+		}
+	}
+}
+
+
