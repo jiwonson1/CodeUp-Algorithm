dd07a5187f40d2100f83992b870601071db44ce5
jiwonson1 <jiwonson.dev@gmail.com>
2021-11-04 12:57:27 +0900

    practice 2차원배열 by java

---
+++ 기초- 2차원 배열/1463 - 2차원 배열 순서대로 채우기1-4.java
@@ -0,0 +1,32 @@
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//StringTokenizer st = new StringTokenizer(br.readLine(), " "); 
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int c=n;
+		
+		//stringTokenizer 
+		
+		int [][] b = new int[n][n]; //2차원 배열 세팅
+		
+			for(int i=0; i<b.length; i++) {
+				for(int j=0; j<b.length; j++) {
+					b[i][j]+=c;
+					System.out.print(b[i][j]+ " ");
+					c+=n;
+				
+				}
+				c=n;
+				c-=i+1;
+				System.out.println();
+			}
+}
+}
