7968549bc37afc70d5175698267492fcf3669c6e
jiwonson1 <jiwonson.dev@gmail.com>
2021-11-06 23:50:02 +0900

    2차원 배열 연습

---
+++ 기초- 2차원 배열/1464 - 2차원 배열 순서대로 채우기 1-5.java
@@ -0,0 +1,19 @@
+﻿import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int[][] c = new int[a][b];
+		int d=a*b;
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < b; j++) {
+				c[i][j]=d;
+				System.out.print(c[i][j] +" ");
+				d--;
+			}
+			System.out.println();
+		}
+	}//main end 
+}
