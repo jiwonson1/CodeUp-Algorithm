adc8c1e5d231be339bd58a0fa5c8fd7bf813e497
jiwonson1 <jiwonson.dev@gmail.com>
2021-11-19 00:00:39 +0900

    1441

---
+++ 기초- 데이터 정렬/1441 버블정렬.java
@@ -0,0 +1,33 @@
+﻿import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int a = scan.nextInt();
+
+    int[] arr = new int[a];
+
+    for(int i = 0; i < arr.length; i++){
+      int b = scan.nextInt();
+
+      arr[i] = b;
+    }
+
+    for(int i = arr.length-1; i > 0; i--){
+      for(int j = 0; j < i; j++){
+        if(arr[j] > arr[j + 1]){
+          int temp = arr[j];
+          arr[j] = arr[j +1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+
+    for(int i = 0; i < arr.length; i++){
+      System.out.printf("%d\n", arr[i]);
+    }
+
+	}
+}
+
+
