347a42db9c78d38f5765b1ef00402b6128a0e51d
jiwonson1 <jiwonson.dev@gmail.com>
2021-11-26 06:43:51 +0900

    알고리즘 연습 삽입정렬

---
+++ 기초- 데이터 정렬/1443- 삽입정렬.java
@@ -0,0 +1,39 @@
+﻿import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class  Main {
+
+
+	    public static void main(String args[]) throws NumberFormatException, IOException{
+	        
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        int a = Integer.parseInt(br.readLine());
+	        
+	        int[] arr = new int[a];
+
+            for(int i = 0; i < arr.length; i++){
+              int b = Integer.parseInt(br.readLine());
+
+              arr[i] = b;
+            }
+        
+            for(int i = arr.length-1; i > 0; i--){
+              for(int j = 0; j < i; j++){
+                if(arr[j] > arr[j + 1]){
+                  int temp = arr[j];
+                  arr[j] = arr[j +1];
+                  arr[j + 1] = temp;
+                }
+              }
+            }
+        
+            for(int i = 0; i < arr.length; i++){
+              System.out.printf("%d\n", arr[i]);
+            }
+        
+        	}
+}
+
