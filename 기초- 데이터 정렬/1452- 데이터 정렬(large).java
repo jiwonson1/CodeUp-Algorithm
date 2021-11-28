63b2e09ec440fe1164e765178a1e94c5210b2933
jiwonson1 <jiwonson.dev@gmail.com>
2021-11-28 09:16:42 +0900

    데이터정렬 연습

---
+++ 기초- 데이터 정렬/1452- 데이터 정렬(large).java
@@ -0,0 +1,29 @@
+﻿import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main{
+   public static  void main (String[]args) throws NumberFormatException, IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[n];
+        
+        for(int i=0; i<arr.length; i++){
+            int a = Integer.parseInt(br.readLine());
+            
+            arr[i] = a;
+        }
+        Arrays.sort(arr);
+        
+        for(int b : arr) {
+        	System.out.println(b);
+        }
+        
+        
+    }
+}
+
