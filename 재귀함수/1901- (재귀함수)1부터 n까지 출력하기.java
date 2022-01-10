a62f27ef4f3e45d22c7fbad5e03e7c89ff53d1bb
jiwonson1 <jiwonson.dev@gmail.com>
2022-01-10 16:17:48 +0900

    practice by java

---
+++ 재귀함수/1901- (재귀함수)1부터 n까지 출력하기.java
@@ -0,0 +1,17 @@
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+static void f(int n) {
+		if(n==0) 
+				return;
+				f(n-1);    
+				System.out.println(n); 
+	}
+	public static void main (String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int b = Integer.parseInt(br.readLine());
+		f(b);
+	}
+}
---
+++ 재귀함수/1902- (재귀함수)1부터 n까지 역순으로 출력하기.java
@@ -0,0 +1,18 @@
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+static void f(int n) {
+		if(n==0) 
+				return;
+		System.out.println(n); 
+				f(n-1);    
+	}
+	public static void main (String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int b = Integer.parseInt(br.readLine());
+		f(b);
+	}
+	
+}
